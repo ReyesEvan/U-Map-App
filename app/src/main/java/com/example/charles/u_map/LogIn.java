848697eca951f284c17b0d08b1d8d271364277aa
JoseGR98 <josemiguelgarcia09@gmail.com>
2018-11-13 20:32:29 -0600

    User LogIn Verification

--- app/src/main/java/com/example/charles/u_map/LogIn.java
+++ app/src/main/java/com/example/charles/u_map/LogIn.java
@@ -1,57 +1,124 @@
 package com.example.charles.u_map;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class LogIn extends AppCompatActivity {
 
     private EditText idInput;
     private EditText passwordInput;
     private Button signIn;
+    private TextView alerts;
 
-    String area = "IA";
-    String room = "101";
+    private String idObtained;
+    private String passObtained;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
 
-        idInput = (EditText) findViewById(R.id.idInputBox);
-        passwordInput = (EditText) findViewById(R.id.passwordInputBox);
-        signIn = (Button) findViewById(R.id.signInButton);
+        idInput =  findViewById(R.id.idInputBox);
+        passwordInput = findViewById(R.id.passwordInputBox);
+        signIn = findViewById(R.id.signInButton);
+        alerts = findViewById(R.id.alertsView);
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateUser(idInput.getText().toString(), passwordInput.getText().toString());
+                try {
+                    confirmInputs();
+                }
+                catch(SQLException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
+    }
+
+    private boolean validateUser() {
+        String field = idInput.getText().toString().trim();
+
+        if (field.isEmpty()) {
+            idInput.setError("Field can not be empty");
+            return false;
+        } else if (!field.matches("[0-9]+")){
+            idInput.setError("Username can only have numbers");
+            return false;
+        } else if (field.length() < 4) {
+            idInput.setError("Username is too short");
+            return false;
+        } else {
+            idInput.setError(null);
+            return true;
+        }
 
     }
 
-    private void validateUser(String id, String password) {
+    private boolean validatePassword() {
+        String field = passwordInput.getText().toString().trim();
 
-       ResultSet result;
+        if (field.isEmpty()) {
+            passwordInput.setError("Field can not be empty");
+            return false;
+        } else {
+            passwordInput.setError(null);
+            return true;
+        }
 
-        if (id.contentEquals("155713") && password.contentEquals("admin")) {
-            Intent goToAreaSelector = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(goToAreaSelector);
+    }
+
+    public void confirmInputs() throws SQLException {
+        String id = idInput.getText().toString();
+        String password = passwordInput.getText().toString();
+        if (validateUser() && validatePassword()) {
+            getUserOfDatabase(id);
+            if ( id.compareTo( idObtained.trim() ) == 0 ) {                      //Si el id esta correcto
+
+                if ( password.compareTo( passObtained.trim() ) == 0 ) {       //Si la contraseña esta correcta
+                    alerts.setText("");
+                    Intent goToAreaSelector = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(goToAreaSelector);
+                }
+                else {
+                    alerts.setText("Incorrect username/password");
+                }
+            }
+            else {
+                alerts.setText("Incorrect username/password");
+            }
+
+        }
+        else{
+            alerts.setText("");
+            Toast.makeText(getApplicationContext(),"Check fields",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void getUserOfDatabase(String id) throws SQLException {
+        DataBase db = new DataBase();
+        ResultSet result;
+
+        result = db.makeQuery("SELECT ID,contraseña FROM Usuarios WHERE CONVERT(VARCHAR,ID) = '" + id + "' ");
+        if (result != null) {
+            idObtained = result.getString("ID");
+            passObtained = result.getString("contraseña");
+            db.closeConnection();
+        }
+        else {
+            System.out.print("La query no encontro nada, por lo que result esta vacio");
         }
     }
+
 }
